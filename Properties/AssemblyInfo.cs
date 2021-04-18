92936c60b1e802b8dc5256e9e55a8a0f260d7375
JeffWiedrich2 <55073690+JeffWiedrich2@users.noreply.github.com>
2021-04-18 16:53:16 -0700

    Add files via upload

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,27 +2,23 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnityEngine.Timeline")]
-[assembly: AssemblyDescription("Unity Timeline")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UnityEditor.Timeline")]
+[assembly: AssemblyDescription("Unity Timeline Editor")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Unity Technologies")]
-[assembly: AssemblyProduct("UnityEngine.Timeline")]
-[assembly: AssemblyCopyright("Copyright � 2016")]
+[assembly: AssemblyProduct("UnityEditor.Timeline")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("Unity.Timeline.Editor")]
 [assembly: ComVisible(false)]
-#if UNITY_EDITOR // RuntimeEditor version
-[assembly: Guid("844F8153-91DB-42D4-9455-CBF1A7BFC434")]
-#else // Runtime version
-[assembly: Guid("6A10B290-9283-487F-913B-00D94CD3FAF5")]
-#endif
+[assembly: Guid("7668179E-DE52-4D63-96C8-B7D84461EA29")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("Assembly-CSharp-testable")]
 [assembly: InternalsVisibleTo("Assembly-CSharp-Editor-testable")]
 [assembly: InternalsVisibleTo("Unity.Timeline.EditorTests")]
 [assembly: InternalsVisibleTo("Unity.Timeline.Tests")]
 [assembly: InternalsVisibleTo("Unity.Timeline.Tests.Common")]
-[assembly: InternalsVisibleTo("Unity.Timeline.Tests.Performance")]
 [assembly: InternalsVisibleTo("Unity.Timeline.Tests.Performance.Editor")]
